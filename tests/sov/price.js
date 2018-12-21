a759fcc614a779c5e5380b73fe0926f4affc1323
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-12-21 12:55:05 -0500

    Update price.js

--- tests/sov/price.js
+++ tests/sov/price.js
@@ -1,9 +1,9 @@
 var request = require("request-promise");
 (async () => {
     var price = await request({
-        url: "http://localhost:8080/sov/price",
+        url: "http://localhost:8080/hth/price",
         headers: {Cookie: "token=admin"}
     });
     /*eslint-disable-next-line no-console*/
-    console.log("The SOV price, according to the server, is " + price + ".");
+    console.log("The HTH price, according to the server, is " + price + ".");
 })();
